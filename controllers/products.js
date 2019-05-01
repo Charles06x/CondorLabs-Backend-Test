8602999ee037def082e4a4323b216110d45e113e
Charles <chad9591@gmail.com>
2019-05-01 02:17:31 -0500

    Products can be created, listed and filter by category. Also you can get a single product.

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,53 @@
 const Product = require('../models/products');
 
 var controller = {
+
+    //Get methods
+    //List all the products in the database (list of products).
+    getAllProducts: function(req, res){
+        Product.find({}).sort('productName').exec((err, products) => {
+            if(err) return res.status(500).send({message: "An error has ocurred.", error: err});
+
+            if(!products) return res.status(404).send({message: "Products not found."});
+
+            return res.status(200).send({products});
+        });
+    },
+
+    //Returns one project, given a name. (Search products and details).
+    getProduct: function(req, res){
+        const productName = req.params.productName;
+
+        //if(productName == null) return res.status(404).send({message: "No name provided."});
+
+        Product.find({productName: productName}).sort('productName').exec((err, product) => {
+            if (err) return res.status(500).send({message: "An error has ocurred.", error: err});
+
+            if(!product) return res.status(404).send({message: "No product was found."});
+
+            return res.status(200).send({product: product});
+        })
+
+    },
+
+    //Returns the products given a category (Display products by category).
+    getProductsByCategory: function(req, res){
+        const productCategory = req.params.productCategory;
+        if(productCategory == null) return res.status(404).send({message: "No category provided."});
+
+        Product.find({productCategory: productCategory}).sort('productName').exec((err, product) => {
+            if (err) return res.status(500).send({message: "An error has ocurred.", error: err});
+
+            if(!product) return res.status(404).send({message: "No product was found."});
+
+            return res.status(200).send({product: product});
+        })
+    },
+
+    
+
+    //Post methods.
+    //Saves a new product to the database.
     saveProduct: function(req, res){
         var product = new Product();
         
--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,11 @@ const ProductController = require('../controllers/products');
 var router = express.Router();
 
 //Get endpoints
+router.get('/products', ProductController.getAllProducts);
+router.get('/categories/:productCategory', ProductController.getProductsByCategory);
+router.get('/products/:productName', ProductController.getProduct);
+
+//Post endpoints
 router.post('/save-product', ProductController.saveProduct);
 
 module.exports = router
